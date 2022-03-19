574c2087ba90195644b7c6a987fa09c08282c532
ahmed-bouhrira <ahmedbouhrira365@gmail.com>
2022-03-19 20:06:54 +0100

    test

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-card color="basil">
+    <v-card-title class="text-center justify-center py-6">
+      <h1 class="tes">
+        NOS VALEURS
+      </h1>
+    </v-card-title>
+
+    <v-tabs v-model="tab" background-color="transparent" grow>
+      <v-tab v-for="item in items" :key="item">
+        {{ item.title }}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item v-for="item in items" :key="item">
+        <v-card flat>
+          <v-card-text>{{ item.text }}</v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tab: null,
+      items: [
+        {
+          title: " SATISFACTION",
+          text:
+            "LE PLAISIR QU’ÉPROUVE NOTRE CLIENTÈLE A VENIR PARTAGER AVEC NOS INSTRUCTEURS QUALIFIÉS EST UN FACTEUR CLÉ DE NOTRE RÉUSSITE. UN CLIENT SATISFAIT SERA UN PARTENAIRE AVEC LEQUEL NOUS DÉVELOPPERONS UNE RELATION DURABLE. "
+        },
+        {
+          title: "DÉCOUVERTE",
+          text:
+            "FAIRE DÉCOUVRIR DES SPORTS EN ÉMERGENCE, DES TECHNIQUES ET DES LIEUX EXCEPTIONNELS DANS NOTRE RÉGION POUR ASSURER UNE EXPÉRIENCE HORS DE L'ORDINAIRE A NOTRE CLIENTÈLE "
+        },
+        {
+          title: " RESPONSABILITÉ ",
+          text:
+            "NOUS SOUHAITONS PARTAGER NOS CONNAISSANCES ET FAIRE BRILLER LE DÉVELOPPEMENT ENTREPRENEURIALE EN INTÉGRANT CERTAINES PRÉOCCUPATION SOCIALES, ENVIRONNEMENTALES ET ECONOMIQUES A NOTRE CULTURE D'ENTREPRISE"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.basil--text {
+  color: #2196f3 !important;
+}
+.tes {
+  font-family: "Shizuru", cursive;
+}
+</style>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Shizuru&display=swap");
+
+@import url("https://fonts.googleapis.com/css2?family=Supermercado+One&display=swap");
+</style>
---
+++ src/components/DownloadSection.vue
@@ -0,0 +1,42 @@
+<template>
+  <section id="download">
+    <template>
+      <v-carousel hide-delimiters>
+        <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src"
+        ></v-carousel-item>
+      </v-carousel>
+    </template>
+  </section>
+</template>
+
+<style scoped>
+#download .container,
+#download .row {
+  height: 100%;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: require("@/assets/img/carou.png")
+        },
+        {
+          src: require("@/assets/img/carou2.png")
+        },
+        {
+          src: require("@/assets/img/carous3.png")
+        },
+        {
+          src: require("@/assets/img/carou4.png")
+        }
+      ]
+    };
+  }
+};
+</script>
